1b864ce4fd9463aa85ca1331e14ab08f4d81c7ac
dukang <625079860@qq.com>
2018-08-15 01:14:30 +0800

    fix middle auth

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -15,9 +15,12 @@ class AdminAuthenticate
      */
     public function handle($request, Closure $next)
     {
-
+        //当未登录，且不在登录页，则跳转到登录页
+        //当登录了，访问登录页，则跳转到首页
         if (!$request->session()->get('admin.login') && $request->route()->getName() !== 'admin.account.login') {
             return redirect(route('admin.account.login'));
+        } elseif ($request->session()->get('admin.login') && $request->route()->getName() == 'admin.account.login') {
+            return redirect(route('admin.index.index'));
         }
         return $next($request);
     }
