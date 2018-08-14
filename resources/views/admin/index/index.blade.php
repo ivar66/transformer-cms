b458e7fab1421485b1804d0f9352a488f1fd4e98
dukang <625079860@qq.com>
2018-08-15 01:24:07 +0800

    delete som code and add some doc

--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -55,49 +55,49 @@
                 </div><!-- /.info-box -->
             </div><!-- /.col -->
         </div>
-       <!--
-        <div class="row">
-            <div class="col-md-12">
-                <div class="box box-default">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">一周用户数据报告</h3>
-                    </div>
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <p class="text-center">
-                                    <strong>一周用户趋势数据</strong>
-                                </p>
-                                <div class="chart">
-                                    <canvas id="user_chart" height="100"></canvas>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="box box-default">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">问答数据报告</h3>
-                    </div>
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <p class="text-center">
-                                    <strong>问题、文章、回答统计</strong>
-                                </p>
-                                <div class="chart">
-                                    <canvas id="question_chart" height="100"></canvas>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>-->
+        <!--
+         <div class="row">
+             <div class="col-md-12">
+                 <div class="box box-default">
+                     <div class="box-header with-border">
+                         <h3 class="box-title">一周用户数据报告</h3>
+                     </div>
+                     <div class="box-body">
+                         <div class="row">
+                             <div class="col-md-12">
+                                 <p class="text-center">
+                                     <strong>一周用户趋势数据</strong>
+                                 </p>
+                                 <div class="chart">
+                                     <canvas id="user_chart" height="100"></canvas>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="row">
+             <div class="col-md-12">
+                 <div class="box box-default">
+                     <div class="box-header with-border">
+                         <h3 class="box-title">问答数据报告</h3>
+                     </div>
+                     <div class="box-body">
+                         <div class="row">
+                             <div class="col-md-12">
+                                 <p class="text-center">
+                                     <strong>问题、文章、回答统计</strong>
+                                 </p>
+                                 <div class="chart">
+                                     <canvas id="question_chart" height="100"></canvas>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>-->
         <div class="row">
             <div class="col-md-12">
                 <div class="box box-default">
@@ -109,9 +109,6 @@
                             <div class="col-md-12">
                                 <table class="table table-striped table-bordered">
                                     <tbody>
-                                    <tr>
-                                        <td>软件版本：{{ config('tipask.version') }} Release {{ config('tipask.release') }} [<a href="http://www.tipask.com/download.html">查看最新版本</a>]</td>
-                                    </tr>
                                     <tr>
                                         <td>服务器域名： {{ $systemInfo['hostName'] }}</td>
                                     </tr>
@@ -156,90 +153,90 @@
             </div>
         </div>
     </section>
-    @endsection
+@endsection
 
 @section('script')
-<script type="text/javascript" src="{{ asset('/static/js/chartjs/chartjs.js') }}"></script>
-{{--<script type="text/javascript">--}}
+    <script type="text/javascript" src="{{ asset('/static/js/chartjs/chartjs.js') }}"></script>
+    {{--<script type="text/javascript">--}}
     {{--$(function(){--}}
-        {{--set_active_menu('root_menu',"{{ route('admin.index.index') }}");--}}
-        {{--var userChart = new Chart($("#user_chart"), {--}}
-            {{--type: 'line',--}}
-            {{--data: {--}}
-                {{--labels: [{!! implode(",",$userChart['labels']) !!}],--}}
-                {{--datasets: [--}}
-                    {{--{--}}
-                    {{--label: '注册数',--}}
-                        {{--backgroundColor: "rgba(51,102,102,0.8)",--}}
-                        {{--borderColor: "rgba(51,102,102,0.8)",--}}
-                        {{--fill: false,--}}
+    {{--set_active_menu('root_menu',"{{ route('admin.index.index') }}");--}}
+    {{--var userChart = new Chart($("#user_chart"), {--}}
+    {{--type: 'line',--}}
+    {{--data: {--}}
+    {{--labels: [{!! implode(",",$userChart['labels']) !!}],--}}
+    {{--datasets: [--}}
+    {{--{--}}
+    {{--label: '注册数',--}}
+    {{--backgroundColor: "rgba(51,102,102,0.8)",--}}
+    {{--borderColor: "rgba(51,102,102,0.8)",--}}
+    {{--fill: false,--}}
 
-                        {{--data: [{{ implode(",",$userChart['registerUsers']) }}]--}}
-                    {{--},--}}
-                    {{--{--}}
-                        {{--fill: false,--}}
+    {{--data: [{{ implode(",",$userChart['registerUsers']) }}]--}}
+    {{--},--}}
+    {{--{--}}
+    {{--fill: false,--}}
 
-                        {{--backgroundColor: "rgba(153,51,51,0.8)",--}}
-                        {{--borderColor: "rgba(153,51,51,0.8)",--}}
+    {{--backgroundColor: "rgba(153,51,51,0.8)",--}}
+    {{--borderColor: "rgba(153,51,51,0.8)",--}}
 
-                        {{--label: '已审核',--}}
-                        {{--data: [{{ implode(",",$userChart['verifyUsers']) }}]--}}
-                    {{--},--}}
-                    {{--{--}}
-                        {{--fill: false,--}}
+    {{--label: '已审核',--}}
+    {{--data: [{{ implode(",",$userChart['verifyUsers']) }}]--}}
+    {{--},--}}
+    {{--{--}}
+    {{--fill: false,--}}
 
-                        {{--backgroundColor: "rgba(153,102,0,0.8)",--}}
-                        {{--borderColor: "rgba(153,102,0,0.8)",--}}
+    {{--backgroundColor: "rgba(153,102,0,0.8)",--}}
+    {{--borderColor: "rgba(153,102,0,0.8)",--}}
 
-                        {{--label: '行家认证',--}}
-                        {{--data: [{{ implode(",",$userChart['authUsers']) }}]--}}
-                    {{--},--}}
-                {{--]--}}
-            {{--},--}}
-            {{--options: {--}}
-                {{--scales: {--}}
-                    {{--yAxes: [{--}}
-                        {{--ticks: {--}}
-                            {{--beginAtZero:true--}}
-                        {{--}--}}
-                    {{--}]--}}
-                {{--}--}}
-            {{--}--}}
-        {{--});--}}
-        {{--var questionChart = new Chart($("#question_chart"), {--}}
-            {{--type: 'bar',--}}
-            {{--data: {--}}
-                {{--labels: [{!! implode(",",$questionChart['labels']) !!}],--}}
-                {{--datasets: [--}}
-                    {{--{--}}
-                    {{--label: '提问',--}}
-                        {{--backgroundColor: "rgba(204,102,51,0.9)",--}}
-                        {{--data: [{{ implode(",",$questionChart['questionRange']) }}]--}}
-                    {{--},--}}
-                    {{--{--}}
-                        {{--label: '回答',--}}
-                        {{--backgroundColor: "rgba(51,102,153,0.9)",--}}
-                        {{--data: [{{ implode(",",$questionChart['answerRange']) }}]--}}
-                    {{--},--}}
-                    {{--{--}}
-                        {{--label: '文章',--}}
-                        {{--backgroundColor: "rgba(0,166,90,0.9)",--}}
-                        {{--data: [{{ implode(",",$questionChart['articleRange']) }}]--}}
-                    {{--},--}}
-                {{--]--}}
-            {{--},--}}
-            {{--options: {--}}
-                {{--scales: {--}}
-                    {{--yAxes: [{--}}
-                        {{--ticks: {--}}
-                            {{--beginAtZero:true--}}
-                        {{--}--}}
-                    {{--}]--}}
-                {{--}--}}
-            {{--}--}}
-        {{--});--}}
+    {{--label: '行家认证',--}}
+    {{--data: [{{ implode(",",$userChart['authUsers']) }}]--}}
+    {{--},--}}
+    {{--]--}}
+    {{--},--}}
+    {{--options: {--}}
+    {{--scales: {--}}
+    {{--yAxes: [{--}}
+    {{--ticks: {--}}
+    {{--beginAtZero:true--}}
+    {{--}--}}
+    {{--}]--}}
+    {{--}--}}
+    {{--}--}}
+    {{--});--}}
+    {{--var questionChart = new Chart($("#question_chart"), {--}}
+    {{--type: 'bar',--}}
+    {{--data: {--}}
+    {{--labels: [{!! implode(",",$questionChart['labels']) !!}],--}}
+    {{--datasets: [--}}
+    {{--{--}}
+    {{--label: '提问',--}}
+    {{--backgroundColor: "rgba(204,102,51,0.9)",--}}
+    {{--data: [{{ implode(",",$questionChart['questionRange']) }}]--}}
+    {{--},--}}
+    {{--{--}}
+    {{--label: '回答',--}}
+    {{--backgroundColor: "rgba(51,102,153,0.9)",--}}
+    {{--data: [{{ implode(",",$questionChart['answerRange']) }}]--}}
+    {{--},--}}
+    {{--{--}}
+    {{--label: '文章',--}}
+    {{--backgroundColor: "rgba(0,166,90,0.9)",--}}
+    {{--data: [{{ implode(",",$questionChart['articleRange']) }}]--}}
+    {{--},--}}
+    {{--]--}}
+    {{--},--}}
+    {{--options: {--}}
+    {{--scales: {--}}
+    {{--yAxes: [{--}}
+    {{--ticks: {--}}
+    {{--beginAtZero:true--}}
+    {{--}--}}
+    {{--}]--}}
+    {{--}--}}
+    {{--}--}}
+    {{--});--}}
 
 
     {{--});--}}
-{{--</script>--}}
+    {{--</script>--}}
 @endsection
--- resources/views/admin/public/footer.blade.php
+++ resources/views/admin/public/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="main-footer text-center">
-    <strong>Copyright &copy; 2010-{{ date('Y') }} <a href="http://www.tipask.com">lovecathy.com</a>.</strong> All rights
+    <strong>Copyright &copy; 2010-{{ date('Y') }} <a href="http://www.lovecathy.com">lovecathy.com</a>.</strong> All rights
     reserved.
 </footer>
