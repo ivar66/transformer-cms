47f50d8bedbf555ed1015cc6d115440bcc4352a1
dukang <625079860@qq.com>
2018-10-08 23:54:54 +0800

    add  sitemap

--- app/Http/Controllers/Web/SiteMapController.php
+++ app/Http/Controllers/Web/SiteMapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use App\Models\ArticleModel;
+use App\Models\TagModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Carbon;
@@ -17,6 +18,7 @@ class SiteMapController extends Controller
         if (!$sitemap->isCached()) {
             /*静态链接*/
             $sitemap->add(URL::to(route('web.blog.index')), null, '1.0', 'daily');
+            $sitemap->add(URL::to(route('web.topic.index')), null, '1.0', 'daily');
 
 
             $startTime = Carbon::now()->subMonth(12);
@@ -27,6 +29,10 @@ class SiteMapController extends Controller
                 $sitemap->add(URL::to(route('web.blog.detail', ['article_id' => $article->id])), $article->created_at, '0.9', 'daily');
             }
 
+            $tags = TagModel::query()->where('created_at', '>', $startTime)->orderBy('created_at', 'desc')->take(1200)->get();
+            foreach ($tags as $tag){
+                $sitemap->add(URL::to(route('web.topic.detail', ['topic_id' => $tag->id])), $tag->created_at, '0.9', 'daily');
+            }
         }
 
         return $sitemap->render('xml');
